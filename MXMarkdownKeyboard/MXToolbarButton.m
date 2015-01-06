61cc69f5523e7fdb748b39c4c22cf16ee16914d6
mexiqq <lijianwei@139.me>
2015-01-06 20:55:59 +0800

    version 0.0.4

--- MXMarkdownKeyboard/MXToolbarButton.m
+++ MXMarkdownKeyboard/MXToolbarButton.m
@@ -11,39 +11,45 @@
 @interface MXToolbarButton ()
 
 @property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSString *imageName;
 @property (nonatomic, copy) eventHandlerBlock buttonPressBlock;
 
 @end
 
 @implementation MXToolbarButton
 
-+ (instancetype)buttonWithTitle:(NSString *)title {
-    return [[self alloc] initWithTitle:title];
++ (instancetype)buttonWithImage:(NSString *)imageName {
+    return [[self alloc] initwithImage:imageName];
 }
 
-- (id)initWithTitle:(NSString *)title {
-    _title = title;
+- (id)initwithImage:(NSString *)imageName{
+    _imageName = imageName;
     return [self init];
 }
 
 - (id)init
 {
-    CGSize sizeOfText = [self.title sizeWithAttributes:@{NSFontAttributeName:[UIFont boldSystemFontOfSize:25.f]}];
-    
-    self = [super initWithFrame:CGRectMake(0, 0, sizeOfText.width + 18.104, sizeOfText.height + 10.298)];
+    self = [super initWithFrame:CGRectMake(0, 0, 40, 40)];
     if (self) {
-        self.backgroundColor = [UIColor colorWithWhite:0.902 alpha:1.0];
+        self.backgroundColor = [self getUIColor:@"FFFFFF"];
         
-        self.layer.cornerRadius = 3.0f;
+        self.layer.cornerRadius = 5.0f;
         self.layer.borderWidth = 1.0f;
-        self.layer.borderColor = [UIColor colorWithWhite:0.8 alpha:1.0].CGColor;
+        self.layer.borderColor = [UIColor whiteColor].CGColor;
         
-        [self setTitle:self.title forState:UIControlStateNormal];
-        [self setTitleColor:[UIColor colorWithWhite:0.500 alpha:1.0] forState:UIControlStateNormal];
+        //设置下阴影
+        CALayer *line = [[CALayer alloc] init];
+        line.borderWidth=1.0f;
+        line.cornerRadius = 5.0f;
+        line.borderColor = [self getUIColor:@"848588"].CGColor;
+        [line setFrame:CGRectMake(0,0,self.layer.bounds.size.width,self.layer.bounds.size.height+1)];
+        [self.layer addSublayer:line];
         
-        self.titleLabel.font = [UIFont boldSystemFontOfSize:25.f];
-        self.titleLabel.textColor = [UIColor colorWithWhite:0.500 alpha:1.0];
+        UIImage *img = [UIImage imageNamed:_imageName];
+        [self setImage:img forState:UIControlStateNormal];
+        
     }
+
     return self;
 }
 
@@ -55,5 +61,26 @@
 - (void)buttonPressed {
     self.buttonPressBlock();
 }
+
+- (UIColor *)getUIColor:(NSString*)hexColorString
+{
+    unsigned int red,green,blue;
+    NSRange range;
+    range.length = 2;
+    
+    range.location = 0;
+    [[NSScanner scannerWithString:[hexColorString substringWithRange:range]]scanHexInt:&red];
+    
+    range.location = 2;
+    [[NSScanner scannerWithString:[hexColorString substringWithRange:range]]scanHexInt:&green];
+    
+    range.location = 4;
+    [[NSScanner scannerWithString:[hexColorString substringWithRange:range]]scanHexInt:&blue];
+    
+    //要进行颜色的RGB设置，要进行对255.0的相除（与其他语言不同）
+    return [UIColor colorWithRed:(float)(red/255.0f)green:(float)(green / 255.0f) blue:(float)(blue / 255.0f)alpha:1.0f];
+    
+}
+
 
 @end
